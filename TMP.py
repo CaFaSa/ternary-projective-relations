05f06cdb76abb6c5a3e481789790d463c3a7b4a7
Fabio Romano <fabiowd@msn.com>
2018-09-27 07:15:34 +0200

    Ultimato l'algoritmo della composition. Manca qualcosa, guardare i commenti nel file TMP.py

--- TMP.py
+++ TMP.py
@@ -6,76 +6,53 @@ from gui.polygon_drawer import PolygonDrawer
 from gui.polygon_drawer_2 import Drawer
 
 
-'''
-def composition(T,rowRelation,columnRelations):
-    columnList=['bt','rs','bf','ls','af']
-    columnRelations=columnRelations.split(":")
-    outputSet=set()
-    tempSet=set()
-    print(len(T.get_subrows(q)))
-    for k in range(len(T.get_subrows(q))):
-        i=0
-        tempSet.clear()
-        while i < len(columnRelations):
-            secondFactor=ProjectiveRelation(T[row][columnList.index(columnRelations[i+1])])
-            if len(tempSet)==0:
-                firstFactor=ProjectiveRelation(T[k][columnList.index(columnRelations[i])])
-                tempSet=_Operations.product(firstFactor,secondFactor)
-            else:
-                tempSet=_Operations.product(tempSet,secondFactor)
-                i=i+1
-        outputSet = outputSet.union(tempSet)
-
-    return outputSet
-'''
 
 def composition(r,q):
     T = Table5_composition()
     columnList = ['bt', 'rs', 'bf', 'ls', 'af']
     subRowsList = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    result = None
-    daMoltiplicare= []
-    risultato=set()
-
+    result = set()
+    
     '''
     Per ogni sottoriga s, salvo in una lista le celle corrispondenti alle relazioni r1,...,rk
     Faccio il prodotto fra di loro, unisco al risultato
     Poi procedo alla sottoriga successiva
+    PROBLEMA: se q è la riga(ed È la riga)...ed r la colonna(ed r È la colonna)
+    richiamando la composition ricorsivamente, dove cavolo li vado a prendere IN e OU sulle COLONNE,
+    visto che non ci sono?
     '''
-    if r.__eq__("in:ou"):
-        pass
+    if "in:ou" in str(r.get_relations()):
+        result=result.union(_Operations.product(composition(ProjectiveRelation("in"),ProjectiveRelation(q)),composition(ProjectiveRelation("ou"),ProjectiveRelation(q))).get_relations())
     else:
         for i in range(len(T.get_subrows(q))):
-            daMoltiplicare= []
+            factors= []
             for rel in r.__repr__().split(":"):
-                daMoltiplicare.append(T.get_value(str(q),subRowsList[i],str(rel)))
-            risultato= risultato.union(moltiplica(daMoltiplicare))
+                factors.append(T.get_value(str(q),subRowsList[i],str(rel)))
+            result = result.union(concatenatedProduct(factors).get_relations())
+
+    return result
 
 
-def moltiplica(listaElementiDaMoltiplicare):
-    risultato=None
-    '''
-    Prende gli elementi e li moltiplica fra loro. 
-    Al primo giro, essendo
-    '''
-    for k in range(0,len(listaElementiDaMoltiplicare)):
-        for singolaRelazioneDelSet in listaElementiDaMoltiplicare[k]:
-            if not risultato:
+def concatenatedProduct(factors):
+    result=None
+    for k in range(0,len(factors)):
+        for factor in factors[k]:
+            if not result:
                 #risultato = risultato.union(ProjectiveRelation(singolaRelazioneDelSet).get_relations())
-                risultato = ProjectiveRelation(singolaRelazioneDelSet)
-
+                result = ProjectiveRelation(factor)
             else:
-                #risultato = risultato.union(_Operations.product(ProjectiveRelation(risultato),ProjectiveRelation(singolaRelazioneDelSet)).get_relations())
-                risultato.product(ProjectiveRelation(singolaRelazioneDelSet))
-                print(risultato)
-    return risultato
+                result = result.product(ProjectiveRelation(factor))
+
+    return result
 
 
 if __name__ == '__main__':
     
-    r = ProjectiveRelation("bt:rs")
+    r = ProjectiveRelation("rs:bf")
     q = ProjectiveRelation("bt")
-    composition(r,q)
+    result=composition(r,q)
+    print("Compose result of", r,"°", q, "is:\n",result)
+
 '''
     result = None
     if r.__eq__("in:ou"):
